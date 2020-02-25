bc092f90fea1429e6ce75601fe9aed12901d9647
kariswr <karinaiswara.ki@gmail.com>
2020-02-25 23:08:44 +0700

    remove some debug code

--- src/paralel_dijkstra.c
+++ src/paralel_dijkstra.c
@@ -134,20 +134,20 @@ int main(int argc, char** argv){
 
     array_short_dis = (int *) malloc(node_per_process*N * sizeof(int));
 
-    printf("%d\n",my_rank);
+    // printf("%d\n",my_rank);
     if(my_rank==0){
         gettimeofday(&start, NULL);
     }
 
-    printf("sebelum for djikstra\n");
+    // printf("sebelum for djikstra\n");
     for(int i_rank=0; i_rank < num_of_process; i_rank++){
         if(i_rank == my_rank){
             for (int i=0; i<node_per_process;i++){
                 short_dis[i] = dijkstra(graf, N, my_rank*node_per_process + i);
-                printf("%d : short_dis[i]=%d\n",my_rank,short_dis[i][0]);
-                for(int j=0; j< N ;j++){
-                    printf("%d : short_dis[%d][%d] : %d \n",my_rank,i,j, short_dis[i][j]);
-                }
+                // printf("%d : short_dis[i]=%d\n",my_rank,short_dis[i][0]);
+                // for(int j=0; j< N ;j++){
+                //     printf("%d : short_dis[%d][%d] : %d \n",my_rank,i,j, short_dis[i][j]);
+                // }
             }
             for (int i=0; i<node_per_process;i++){
                 for (int j=0; j<N; j++){
@@ -156,33 +156,33 @@ int main(int argc, char** argv){
             }
         }
     }
-    printf("%d: selesai djisktra\n",my_rank);
+    // printf("%d: selesai djisktra\n",my_rank);
 
-    printf("%d : last_idx = %d\n",my_rank, N*node_per_process-1);
-    printf("node + N %d %d \n", node_per_process, N);
-    for(int j=0; j< node_per_process* N ;j++){
-        printf(" %d ", array_short_dis[j]);
-    }
-    printf("\n%d : array short dis (last_idx) : %d\n",my_rank, array_short_dis[N*node_per_process-1]);
+    // printf("%d : last_idx = %d\n",my_rank, N*node_per_process-1);
+    // printf("node + N %d %d \n", node_per_process, N);
+    // for(int j=0; j< node_per_process* N ;j++){
+    //     printf(" %d ", array_short_dis[j]);
+    // }
+    // printf("\n%d : array short dis (last_idx) : %d\n",my_rank, array_short_dis[N*node_per_process-1]);
 
     //printMatrix(graf, N);
 
     if (my_rank==0){
         
-        printf("masuk my_rank 0\n");
+        // printf("masuk my_rank 0\n");
         //short_dis = (int **)malloc(node_per_process * sizeof(int*));
         //for(int i = 0; i < node_per_process; i++) short_dis[i] = (int *)malloc(N * sizeof(int));
 
         //global_short_dis = (int **)malloc(N * sizeof(int*));
         //for(int i = 0; i < N; i++) global_short_dis[i] = (int *)malloc(N * sizeof(int));
         global_short_dis = (int *)malloc(N * N * sizeof(int));        
-        printf("beres inisialisasi global short dis\n");
+        // printf("beres inisialisasi global short dis\n");
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Gather(array_short_dis, node_per_process * N, MPI_INT, global_short_dis, node_per_process * N, MPI_INT, 0, MPI_COMM_WORLD);
         
-    printf("beres gather \n");
+    // printf("beres gather \n");
 
     if(my_rank==0){
         gettimeofday(&end, NULL);
